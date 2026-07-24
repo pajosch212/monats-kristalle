ff2ece1c9e9b1a807ff3c7736b1e49d2a3f905da
pajosch <pajosch@web.de>
2026-07-25 01:22:43 +0200

    Add Google Tag Manager (GTM-5TH2T6XZ)

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de" className={`${fraunces.variable} ${workSans.variable} h-full antialiased`}>
+      <head>
+        <Script id="gtm" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5TH2T6XZ');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5TH2T6XZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-R9YCF808LV" strategy="afterInteractive" />
         <Script id="gtag-init" strategy="afterInteractive">
           {`
